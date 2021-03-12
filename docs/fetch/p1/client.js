edfa3d41b9b123f94e141d85383388d6f71b4768
DanielLoRo31 <daniel.lozano.rodriguez2000@gmail.com>
2021-03-12 16:41:50 -0600

    add: practica 1

---
+++ docs/fetch/p1/client.js
@@ -0,0 +1,15 @@
+var paises = document.getElementById("paises");
+
+fetch("datos.json").then((response) => {
+  response.json().then((datos) => {
+    datos.forEach((registro) => {
+      let h2 = document.createElement("h2");
+      let span = document.createElement("span");
+      span.textContent = "País: "
+      h2.appendChild(span);
+      h2.textContent =
+        registro.country + ", casos:" + registro.cases;
+      paises.appendChild(h2);
+    });
+  });
+});
